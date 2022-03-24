457f51fbd27c30c0a4a7a3f73b5d7ea5f0ee948a
queavos <queavos.dev@gmail.com>
2022-03-24 18:15:58 -0300

    dia 3

---
+++ 0317/js/app.js
@@ -0,0 +1,24 @@
+// function carga()
+// {
+
+  console.log("test") ;
+  var valor=5;
+  console.log(valor);
+//  valor="philipe"+1;
+  valor=valor+1
+    console.log(valor);
+    if (valor==1) {
+      console.log(valor);
+    } else {
+      console.log(valor);
+    }
+
+    while (valor>1) {
+       valor--;
+       console.log("en while");
+    }
+    for (var i = 0; i < 5; i++) {
+       console.log("en for");
+    }
+// }
+  //alert("aler");
